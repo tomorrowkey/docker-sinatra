13383d914835ae582365139b91a4be7d401e8d2e
Tomoki Yamashita <tomoki-yamashita@cookpad.com>
2016-10-17 22:41:49 +0900

    Install haml

--- app.rb
+++ app.rb
@@ -1,13 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  <<~HTML
-    <!DOCTYPE html>
-    <html>
-      <head><title>Hello, World.</title></head>
-      <body>
-        <h1>Hello, World.</h1>
-      </body>
-    </html>
-  HTML
+  haml :index
 end
