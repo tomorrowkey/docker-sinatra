118c955bef949e1fbf78ab9e14e61c5a90c55928
Tomoki Yamashita <tomoki-yamashita@cookpad.com>
2016-10-17 22:37:07 +0900

    Add simple sinatra app

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get '/' do
+  <<~HTML
+    <!DOCTYPE html>
+    <html>
+      <head><title>Hello, World.</title></head>
+      <body>
+        <h1>Hello, World.</h1>
+      </body>
+    </html>
+  HTML
+end
